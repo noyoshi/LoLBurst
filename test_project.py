a07bff33bc55250689882fc674caceb3a599b01b
silver <noahcy117@gmail.com>
2018-04-24 15:29:05 -0400

    more template testing added

--- test_project.py
+++ test_project.py
@@ -4,10 +4,13 @@ import os
 import project 
 import unittest 
 from flask_testing import TestCase
-from flask import Flask
+from flask import Flask, jsonify
 
 class ProjectTestCase(TestCase):
-
+    '''
+    Testing Suite for LoLBurst, for use in Travis CI 
+    '''
+    
     def create_app(self): 
         app = Flask(__name__)
         app.config['TESTING'] = True 
@@ -21,7 +24,20 @@ class ProjectTestCase(TestCase):
         rv = self.app.get('/')
         self.assert_template_used('item.html')
 
+    # Why the fuck does this not work
+    '''
+    def test_backend(self):
+        resp = self.client.get('/backend')
+        self.assertEquals(resp.json, jsonify({"Please":"Work", "Derp": "SOWWY"}))
+    '''
 
+    def test_info_html(self):
+        rv = self.app.get('/info')
+        self.assert_template_used('info.html')
+
+    def test_stats_html(self):
+        rv = self.app.get('/stats')
+        self.assert_template_used('stats.html')
 
 if __name__ == '__main__': 
     unittest.main()
