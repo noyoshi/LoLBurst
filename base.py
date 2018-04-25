8e3e78a95ade79dfc9d4f802715a0a34b170d930
Timothy Blazek <tblazek@nd.edu>
2018-04-24 22:11:25 -0400

    Trying a new Trie class

--- base.py
+++ base.py
@@ -77,50 +77,117 @@ class SortedList(Base):
         #                 break
         #         self.data.insert(j, elm)
 
+# class Node():
+#     def __init__(self):
+#         self.string = None
+#         self.children = dict()
+#
+#     def insert(self, word, index=0):
+#         # Gets the current letter of the word wanting to be added
+#         curr = word[index]
+#         # checks if that letter is already a child
+#         if curr not in self.children:
+#             self.children[curr] = Node()
+#
+#         if index + 1 == len(word):
+#             #if last letter in the word, then update the string value of the Node class
+#             self.children[curr].string = word
+#         else:
+#             #if not the last letter, you want to insert the rest of the word to the tree
+#             self.children[curr].insert(word, index+1)
+#
+#     def get_trie(self, search, index):
+#         x = [] #initialize empty list
+#         for key, value in self.children.items(): #goes through all the children
+#         #checks to see if on the right level of the Trie or if the key is the same as the current letter
+#             if index >= len(search) or key.lower() == search[index].lower():
+#                 if value.string and value.string.startswith(search): #Adds the word to the list
+#                     x.append(value.string)
+#                 if bool(value.children): #if there are children
+#                     if index < len(search):
+#                         x += value.get_trie(search, index + 1)
+#                     else:
+#                         x += value.get_trie(search, index)
+#
+#         return x
+#
+# class Trie():
+#     def __init__(self):
+#         self.root = Node()
+#
+#     def insert(self, data):
+#         for elm in data:
+#             self.root.insert(elm)
+#
+#     def search(self,data, index=0):
+#         return self.root.get_trie(data, index)
+
 class Node():
-    def __init__(self):
-        self.string = None
-        self.children = dict()
+    def __init__(self, val):
+        self.value = val
+        self.children = []
+        self.word = None
 
     def insert(self, word, index=0):
-        # Gets the current letter of the word wanting to be added
-        curr = word[index]
-        # checks if that letter is already a child
-        if curr not in self.children:
-            self.children[curr] = Node()
+        original = word
+        wordnew = word[index:]
+        if len(wordnew) > 0:
+            s = wordnew[0]
+            wordnew = wordnew[1:]
+            set = False
+            for char in self.children:
+                if char.value == s:
+                    char.insert(original, index+1)
+                    set = True
+                    break
+            if not set:
+                tmp = Node(s)
+                self.children.append(tmp)
+                self.insert(original, index)
 
-        if index + 1 == len(word):
-            #if last letter in the word, then update the string value of the Node class
-            self.children[curr].string = word
         else:
-            #if not the last letter, you want to insert the rest of the word to the tree
-            self.children[curr].insert(word, index+1)
+            self.word = original
 
-    def get_trie(self, search, index):
-        x = [] #initialize empty list
-        for key, value in self.children.items(): #goes through all the children
-        #checks to see if on the right level of the Trie or if the key is the same as the current letter
-            if index >= len(search) or key.lower() == search[index].lower():
-                if value.string and value.string.startswith(search): #Adds the word to the list
-                    x.append(value.string)
-                if bool(value.children): #if there are children
-                    if index < len(search):
-                        x += value.get_trie(search, index + 1)
-                    else:
-                        x += value.get_trie(search, index)
+    def search(self, pref):
+        curr = pref
+        node = self
+        x = []
+        while len(curr) > 0:
+            set = False
+            for char in node.children:
+                if char.value.lower() == curr[0].lower():
+                    node = char
+                    curr = curr[1:]
+                    set = True
+                    break
+            if not set:
+                return x
+        if node.word:
+            x.append(node.word)
+        x = node.r_child(x)
+        return x
+
+    def r_child(self, x):
+        currlist = self.children
+        if len(currlist) > 0:
+            for child in currlist:
+                if child.word:
+                    x.append(child.word)
+                x = child.r_child(x)
 
         return x
 
+
 class Trie():
     def __init__(self):
-        self.root = Node()
+        self.root = Node("")
 
     def insert(self, data):
         for elm in data:
             self.root.insert(elm)
 
-    def search(self,data, index=0):
-        return self.root.get_trie(data, index)
+    def search(self,data):
+        return self.root.search(data)
 
 if __name__ == '__main__':
     initialize_champs()
