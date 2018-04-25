a9c7049b5d74a4ebc086d64c978a079bfbcd57e4
silver <noahcy117@gmail.com>
2018-04-25 12:56:04 -0400

    benchmarking update - now with memory usage as well

--- testme.sh
+++ testme.sh
@@ -1,29 +1,47 @@
-#!/bin/sh 
+#!/bin/bash 
+
+use_temp () {
+    cat temp_r | head -n2 
+    cat temp_r | tail -n1 | cut -f2 -d$'\t'  
+    rm temp_r
+    echo ""
+}
+
 
 chmod +x benchmark.py
 make 
 
-echo "\nTesting sorted"
+echo 'Testing search time'
+echo ""
+echo 'Testing sorted'
+echo ""
 echo "=============="
 cat /usr/share/dict/american-english >> temp_f.txt
 cat /usr/share/dict/catala >> temp_f.txt
 cat /usr/share/dict/danish >> temp_f.txt
 echo "a"
-cat temp_f.txt | ./measure ./benchmark.py -s "a" | tail -n1
-echo "\nka"
-cat temp_f.txt | ./measure ./benchmark.py -s "ka" | tail -n1
+cat temp_f.txt | ./measure ./benchmark.py -s "a" > temp_r
+use_temp
+echo "ka"
+cat temp_f.txt | ./measure ./benchmark.py -s "ka" > temp_r 
+use_temp
 
-echo "\nTesting unsorted"
+echo 'Testing unsorted'
 echo "================"
 echo "a"
-cat temp_f.txt | ./measure ./benchmark.py -u "a" | tail -n1
-echo "\nka"
-cat temp_f.txt | ./measure ./benchmark.py -u "ka" | tail -n1
+cat temp_f.txt | ./measure ./benchmark.py -u "a" > temp_r
+use_temp
+echo "ka"
+cat temp_f.txt | ./measure ./benchmark.py -u "ka" > temp_r
+use_temp
 
-echo "\nTesting trie"
+echo "Testing trie"
 echo "============"
 echo "a"
-cat temp_f.txt | ./measure ./benchmark.py -t "a" | tail -n1
-echo "\nka"
-cat temp_f.txt | ./measure ./benchmark.py -t "ka" | tail -n1
+cat temp_f.txt | ./measure ./benchmark.py -t "a" > temp_r
+use_temp
+echo "ka"
+cat temp_f.txt | ./measure ./benchmark.py -t "ka" > temp_r
+use_temp
+
 rm temp_f.txt
